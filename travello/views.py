3c0e7e07402a780d47d6bcbd4345b67368f967d7
Nasim <nasimnu14@gmail.com>
2020-10-12 23:14:49 +0600

    Place description can be changed by program

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    dest1=Destination()
+    dest1.description="This is not a googd place"
+    dest1.name="Dhaka"
+    dest1.price=1000
+
+    return render(request,"index.html",{'dest1':dest1})
