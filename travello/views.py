b6ece95bac64da8a41f32c69eb777099af24a752
Nasim <nasimnu14@gmail.com>
2020-10-12 23:45:42 +0600

    Place image can be changed from views function

--- travello/views.py
+++ travello/views.py
@@ -5,8 +5,23 @@ from .models import Destination
 
 def index(request):
     dest1=Destination()
+    dest2=Destination()
+    dest3=Destination()
+
     dest1.description="This is not a googd place"
     dest1.name="Dhaka"
     dest1.price=1000
+    dest1.img="destination_1.jpg"
 
-    return render(request,"index.html",{'dest1':dest1})
+    dest2.name="Chottagram"
+    dest2.description="Very bad"
+    dest2.price=1500
+    dest2.img="destination_2.jpg"
+
+    dest3.name="B-Baria"
+    dest3.description="Very Overall"
+    dest3.price=100
+    dest3.img="destination_3.jpg"
+    dests=[dest1,dest2,dest3]
+
+    return render(request,"index.html",{'dests':dests})
